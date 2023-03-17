5344654444bd6f1fe029584f08bb8627a04d5804
kshitijaingle <108507770+kshitijaingle@users.noreply.github.com>
2023-03-17 14:14:42 +0530

    Add files via upload

---
+++ word_count_pyspark.py
@@ -0,0 +1,11 @@
+import pyspark
+import sys
+sc=pyspark.SparkContext('Document[*]')
+filepath=str(sys.argv[1])
+txt=sc.textFile(filepath)
+print(txt.count())
+python_lines=txt.filter(lambda line: 'limeguru' in line.lower())
+limeguru_count=python_lines.count()
+print("++++++++++++++++++++++++++++")
+print("limeguru word count:"+str(limeguru_count))
+print("++++++++++++++++++++++++++++")
